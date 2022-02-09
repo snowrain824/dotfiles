3f16a4701bc5d34ae73cd02d0b186221fc2cb47b
chance_snow <90690379+snowrain824@users.noreply.github.com>
2022-02-09 15:50:39 +0900

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,44 +1,85 @@
-(setq inhibit-startup-message t)
-
-(setq make-backup-files nil)
-;; tramp auto-save auto-backup dir
-(setq tramp-auto-save-directory "~/.emacs.d/tramp-autosave")
-(setq auto-save-file-name-transforms
-      '((".*" "~/.emacs.d/auto-saves/" t)))
-
+;; Default settings
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+(scroll-bar-mode 0)
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+(setq delete-auto-save-files t)
+(save-place-mode 1)
+(setq inhibit-startup-message t)
 
-;; tab = 4spaces & offset size = 4spaces
-(setq default-tab-width 4)
-(setq indent-tabs-mode nil)
-(defvaralias 'c-basic-offset 'default-tab-width)
+(savehist-mode 1)
 
+;; for mac to run shell env
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize)
+;; set meta key to command
+  (setq mac-command-modifier 'meta))
+
+;; frame-size
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 120))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist 
+         (cons 'height (/ (- (x-display-pixel-height) 200)
+                             (frame-char-height)))))))
+(set-frame-size-according-to-resolution)
+
+;; encoding
+(set-language-environment 'utf-8)
+(set-file-name-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; global key-bindings
+(global-set-key (kbd "M-:") 'comment-dwim)
+
+
+;; kill whole line
+(setq kill-whole-line t)
+
+;; display line number
 (progn 
   (global-display-line-numbers-mode)
+  (setq display-line-numbers-type 'relative)
   (set-face-attribute 'line-number-current-line nil
 		      :foreground "gold"))
+(setq column-number-mode t)
 
-(setq x-select-enable-clipboard t)
-
-;; font
-(set-face-attribute 'default nil
-		    :family "Hack"
-		    :height 160
-		    :weight 'normal
-		    :width 'normal) 
+(defvar kang/indent-width 2)
+(setq indent-tabs-mode nil)
+(defvaralias 'c-basic-offset 'default-tab-width)
 
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	     '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
 
-;; Bootstrap 'use-package'
+;; Bootstrap 'use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+	use-package-expand-minimally t))
 
 (use-package try
   :ensure t)
@@ -47,51 +88,59 @@
   :ensure t
   :config (which-key-mode))
 
-(use-package smartparens
+;;(use-package dracula-theme
+;;  :ensure t
+;;  :config (load-theme 'dracula t)
+;;  (set-frame-font "Iosevka-20"))
+(use-package darcula-theme
   :ensure t
-  :config (smartparens-global-mode))
-;;(require 'smartparens-config)
-;;(smartparens-global-mode t)
-
-;(use-package smartparens
-;  :ensure t
-;  :config (smartparens-config))
-
-;;(use-package srcery-theme
-;;  nnn:ensure t
-;;  :config (load-theme 'srcery t))
-
-(use-package dracula-theme
-  :ensure t
-  :config (load-theme 'dracula t))
+  :config (load-theme 'darcula t)
+  (set-frame-font "Inconsolata-20"))
+;;  (set-frame-font "Iosevka-20"))
+;;
 ;;(use-package zenburn-theme
 ;;  :ensure t
 ;;  :config (load-theme 'zenburn t))
-
-;; Org-mode stuff
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+;;
+(require 'smartparens-config)
+(smartparens-global-mode)
 
 ;; Ido mode
 (setq indo-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
 
-;;(defalias 'list-buffers 'ibuffer)
-(defalias 'list-buffers 'ibuffer-other-window)
+;; cc-vars
+(use-package cc-vars
+  :ensure nil
+  :config
+  (setq c-default-style '((java-mode . "java")
+			  (awk-mode . "awk")
+			  (c++-mode . "bsd")
+			  (c-mode . "bsd")
+			  (other . "k&r")))
+  (setq-default c-basic-offset kang/indent-width))
+	
 
-;; ace-window
-(use-package ace-window
-  :ensure t
-  :init
-  (progn
-    (global-set-key [remap other-window] 'ace-window)
-    (custom-set-faces
-     '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-    ))
+;; spell checking
+(use-package flyspell
+  :config
+  (add-hook 'c-mode-hook '(lambda()
+			    (flyspell-prog-mode)))
+  (add-hook 'c++-mode-hook '(lambda()
+			      (flyspell-prog-mode)))
+  (add-hook 'go-mode-hook '(lambda()
+			     (flyspell-prog-mode)))
+  (add-hook 'rustic-mode-hook '(lambda()
+				 (flyspell-prog-mode)))
+  (add-hook 'python-mode-hook '(lambda()
+				 (flyspell-prog-mode))))
+
+;; python-mode
+;; (use-package python-black
+;;   :demand t
+;;   :after python
+;;   :hook (python-mode . python-black-on-save-mode-enable-dwim))
 
 ;; company-mode
 (use-package company
@@ -99,52 +148,104 @@
   :defer t
   :init (add-hook 'after-init-hook 'global-company-mode)
   :config
-  (use-package company-irony :ensure t :defer t)
-  (setq company-idle-delay nil
-	company-mininum-prefix-length 2
+  (setq company-idle-delay 0.5
+	company-minimum-prefix-length 1
+	company-selection-wrap-around t
 	company-show-numbers t
-	company-tooltip-limit 20
-	company-dabbrev-downcase nil
-	company-backends '((company-irony))
+	company-tooltip-limit 12
 	)
-  :bind ("C-;" . company-complete-common))
+  (setq company-tooltip-minimum-width 60)
+  (setq company-tooltip-margin 60)	
+  (setq company-tooltip-align-annotations t)
+  :bind
+  (:map company-active-map
+	("C-n" . company-select-next)
+	("C-p" . company-select-previous)
+	("M-<" . company-select-first)
+	("M->" . company-select-last)
+	("C-;" . company-complete-common)))
 
-;; elpy
-(use-package elpy
+
+(use-package company-box
+  :hook (company-mode . company-box-mode)
+  :config
+  (add-to-list 'company-box-frame-parameters '(font . "Hack-20")))
+
+
+;; lsp-mode
+;;(use-package lsp-mode
+;;  :ensure t
+;;  :init (yas-global-mode)
+;;  :hook ((rust-mode . lsp)
+;;        (go-mode .lsp-deferred))
+;;  :bind ("C-c h" . lsp-describe-thing-at-point)
+;;  :custom (lsp-rust-server 'rust-analyzer))
+;; (use-package lsp-ui
+;;   :ensure t
+;;   :config
+;;   ;;(lsp-ui-doc-position 'at-point)
+;;   ;;(lsp-ui-doc-max-width 120)
+;;   ;;(lsp-ui-doc-max-height 30)
+;;   (setq lsp-ui-doc-enable nil)
+;;   (setq lsp-ui-doc-header t)
+;;   (setq lsp-ui-doc-include-signature t)
+;;   (setq lsp-ui-sideline-show-code-actions nil)
+;;   (setq lsp-ui-sideline-show-diagnostics t)
+;;   (setq lsp-ui-sideline-delay 0.5))
+
+(use-package eglot
   :ensure t
-  :init
-  (elpy-enable))
-
-;; python-mode-hooks
-(add-hook 'python-mode-hook
-	  (lambda()
-	    (setq indent-tabs-mode t)
-	    (setq tab-width 4)
-	    (setq python-indent-offset 4)))
+  :config
+  (add-to-list 'eglot-server-programs '(c-mode . ("clangd")))
+  (add-to-list 'eglot-server-programs '(c++-mode . ("clangd")))
+  (add-to-list 'eglot-server-programs '(go-mode . ("gopls")))
+  (add-to-list 'eglot-server-programs '(rustic-mode . ("rust-analyzer")))
+  (add-to-list 'eglot-server-programs '(python-mode . ("pyls")))
+  (add-hook 'eglot--managed-mode-hook (lambda() (flymake-mode -1)))
+  (add-hook 'c-mode-hook 'eglot-ensure)
+  (add-hook 'c++-mode-hook 'eglot-ensure)
+  (add-hook 'go-mode-hook 'eglot-ensure)
+  (add-hook 'rustic-mode-hook 'eglot-ensure)
+  (add-hook 'python-mode-hook 'eglot-ensure)
+  ;; format on save
+  (add-hook 'c-mode-hook '(lambda() (add-hook 'before-save-hook 'eglot-format-buffer nil t)))
+  (add-hook 'c++-mode-hook '(lambda() (add-hook 'before-save-hook 'eglot-format-buffer nil t)))
+  (add-hook 'python-mode-hook '(lambda() (add-hook 'before-save-hook 'eglot-format-buffer nil t)))
+  (define-key eglot-mode-map (kbd "C-c r") 'eglot-rename)
+  (define-key eglot-mode-map (kbd "C-c f") 'eglot-format))
 
 ;; go-mode
 (use-package go-mode
   :ensure t
+  :defer t
+  :mode ("\\.go$" . go-mode)
   :config
+  (setq gofmt-command "goimports")
   (add-hook 'before-save-hook 'gofmt-before-save))
 
-(add-hook 'go-mode-hook
-	  (lambda()
-	    (setq indent-tabs-mode t)
-	    (setq tab-width 4)
-	    (setq go-indent-offset 4)))
-
-
-;; irony-mode
-(use-package irony
+;; config for rust-mode
+(use-package rustic
   :ensure t
   :defer t
-  :init
-  (add-hook 'c++-mode-hook 'irony-mode)
-  (add-hook 'c-mode-hook 'irony-mode)
-  (add-hook 'objc-mode-hook 'irony-mode)
+  :mode ("\\.rs$" . rustic-mode)
   :config
-  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+  (setq rustic-lsp-client 'eglot)
+  (setq lsp-eldoc-hook nil)
+  (setq lsp-signature-auto-activate nil)
+  (setq lsp-enable-symbol-highlighting nil)
+  (setq-default rustic-format-trigger 'on-save))
+  
+;; geiser
+(use-package geiser-guile
+  :ensure t)
+
+
+;; move text
+(use-package move-text
+  :ensure t
+  :config
+  (global-set-key (kbd "M-p") 'move-text-up)
+  (global-set-key (kbd "M-n") 'move-text-down))
 
 
 (custom-set-variables
@@ -152,13 +253,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("c1284dd4c650d6d74cfaf0106b8ae42270cab6c58f78efc5b7c825b6a4580417" default))
  '(package-selected-packages
-   '(company-irony go-mode Dracula company-jedi jedi smartparens dracula-theme ace-window which-key use-package try srcery-theme org-bullets)))
+   '(move-text zenburn-theme darcula-theme darcula zenburn exec-path-from-shell company-box python-black go-mode dracula-theme which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ )
